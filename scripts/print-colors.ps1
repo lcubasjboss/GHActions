fa432418e1bac265522d914db3a5cc990f6c12a2
Luis Cubas <marcus.zero2007@gmail.com>
2025-07-03 01:38:23 -0500

    Update

--- scripts/print-colors.ps1
+++ scripts/print-colors.ps1
@@ -6,3 +6,14 @@ Write-Host "Esta es la tercera línea en amarillo." # Esto se mostrará en blanc
 Write-Host "::notice title=Línea Azul::Esta es la primera línea que se verá con un fondo azul claro en el log de GitHub Actions."
 Write-Host "::warning title=Línea Roja::Esta es la segunda línea que se verá con un fondo amarillo en el log de GitHub Actions (advertencia)."
 Write-Host "::error title=Línea Amarilla::Esta es la tercera línea que se verá con un fondo rojo en el log de GitHub Actions (error)."
+
+# Definir códigos ANSI para los colores
+$Blue = "`e[34m"
+$Red = "`e[31m"
+$Yellow = "`e[33m"
+$Reset = "`e[0m" # Código para resetear el color a por defecto
+
+# Imprimir las líneas usando los códigos ANSI
+Write-Output "${Blue}Esta es la primera línea en azul.${Reset}"
+Write-Output "${Red}Esta es la segunda línea en rojo.${Reset}"
+Write-Output "${Yellow}Esta es la tercera línea en amarillo.${Reset}"
