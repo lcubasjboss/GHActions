e43ae7694ffe150eb4716dc2c6d16b616e247a05
Luis Cubas <marcus.zero2007@gmail.com>
2025-07-03 01:32:54 -0500

    Update

---
+++ scripts/print-colors.ps1
@@ -0,0 +1,3 @@
+Write-Host "Esta es la primera línea en azul." -ForegroundColor Blue
+Write-Host "Esta es la segunda línea en rojo." -ForegroundColor Red
+Write-Host "Esta es la tercera línea en amarillo." -ForegroundColor Yellow
